7d1f12420ab909241c9e47314e987001ca920086
G-R3 <gr261212@gmail.com>
2026-03-11 15:53:55 -0400

    clean and format

--- lcd.ino
+++ lcd.ino
@@ -1,6 +1,6 @@
 #include <LiquidCrystal.h>
 
-const int pauseStatePin = 6;
+const int pauseBtnPin = 6;
 
 const int rs = 12;
 const int enable = 11;
@@ -11,43 +11,42 @@ const int d7 = 2;
 
 LiquidCrystal lcd(rs, enable, d4, d5, d6, d7);
 
-
-unsigned long start;
+unsigned long startTime;
 unsigned long passedTime;
 // const long focusTime = 1500000 // 25 minutes
-const long focusTime = 5000; // 5 seconds
-const long breakTime = 4000; // 4 seconds;
+const long focusTime = 5000;  // 5 seconds
+const long breakTime = 4000;  // 4 seconds;
 
 long remainingTime;
 
 bool focusMode = true;
 
-int pauseState = 0;
+int currentPauseState = 0;
 int prevPauseState = 0;
-int timerEnabled = 1;
+int timerRunning = 1;
 
-
-unsigned long timeAtPaused;
+unsigned long timerPausedAt;
 
 void setup() {
   lcd.begin(16, 2);
 
-  start = millis();
+  startTime = millis();
 
   Serial.begin(9600);
+
+  pinMode(pauseBtnPin, INPUT);
 }
 
-
 void render(int minutes, int seconds, bool focusMode) {
-  lcd.setCursor(0,0);
+  lcd.setCursor(0, 0);
 
-  if(focusMode) {
+  if (focusMode) {
     lcd.print("Focus Time");
   } else {
     lcd.print("Break Time");
   }
 
-  lcd.setCursor(0,1);
+  lcd.setCursor(0, 1);
   lcd.print(minutes);
   lcd.print(":");
 
@@ -57,71 +56,68 @@ void render(int minutes, int seconds, bool focusMode) {
 
 
   lcd.setCursor(5, 1);
-  if (!timerEnabled) {
+  if (!timerRunning) {
     lcd.print("PAUSED");
   } else {
     lcd.print("      ");
   }
-  
 }
 
+void toggleTimer(int currentPauseState) {
+  Serial.println("===== Attempting to toggle timer =====");
+  Serial.print("Timer running: ");
+  Serial.print(timerRunning);
+  Serial.print(" Pause state: ");
+  Serial.println(currentPauseState);
 
-long getRemainingTime(bool focusMode) {
+  if (currentPauseState == 1 && prevPauseState == 0) {
+    timerPausedAt = millis();
+    timerRunning = !timerRunning;
+  }
+}
+
+long getRemainingTime() {
   long remainingTime;
 
-  if(focusMode) {
-    remainingTime = focusTime - (millis() - start);
+  if (focusMode) {
+    remainingTime = focusTime - (millis() - startTime);
   } else {
-    remainingTime = breakTime - (millis() - start);
+    remainingTime = breakTime - (millis() - startTime);
   }
 
   return remainingTime;
 }
 
-
 void loop() {
-  pauseState = digitalRead(pauseStatePin);
-  Serial.println(pauseState);
+  currentPauseState = digitalRead(pauseBtnPin);
+  toggleTimer(currentPauseState);
 
-  if(pauseState == 1 && prevPauseState == 0) {
-    timeAtPaused = millis();
-    timerEnabled = !timerEnabled;
-  } 
+  if (timerRunning) {
+    startTime = startTime + (millis() - timerPausedAt);
 
+    remainingTime = getRemainingTime();
 
-
-  if(timerEnabled) {
-
-    remainingTime = getRemainingTime(focusMode);
-    
-    if(remainingTime <= 0) {
+    if (remainingTime <= 0) {
       remainingTime = 0;
-      start = millis();
+      startTime = millis();
       focusMode = !focusMode;
     }
   }
 
-
-
-
-
-
   long totalSeconds = (remainingTime) / 1000;
   int seconds = totalSeconds % 60;
   int minutes = totalSeconds / 60;
+
   render(minutes, seconds, focusMode);
 
-  prevPauseState = pauseState;
- 
+  prevPauseState = currentPauseState;
 }
-
-
 
 
 /**
   LCD pomodoro
   - Menu
-    - start
+    - startTime
     - configure
       - Set focus duration
       - Set break duration
